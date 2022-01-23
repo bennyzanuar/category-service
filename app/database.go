92c0a21dd1abe7bc136f298910c3c33ae14e2814
Benny Zanuarwan PG <benny.zanuarwan@tanihub.com>
2022-01-23 14:54:30 +0700

    make it better

---
+++ app/database.go
@@ -0,0 +1,21 @@
+package app
+
+import (
+	"category-service/helper"
+	"database/sql"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func NewDB() *sql.DB {
+	db, err := sql.Open("mysql", "root:123qweasd@tcp(localhost:3306)/go_category_service")
+	helper.PanicIfError(err)
+
+	db.SetMaxIdleConns(5)
+	db.SetMaxOpenConns(20)
+	db.SetConnMaxLifetime(60 * time.Minute)
+	db.SetConnMaxIdleTime(10 * time.Minute)
+
+	return db
+}
